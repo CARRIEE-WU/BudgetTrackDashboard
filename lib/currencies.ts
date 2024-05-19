beaf1dd033843dc2fb0a72a994ed3b0fc4a91c00
CARRIEE-WU <a945120@gmail.com>
2024-05-19 18:56:44 +0800

    first commit

---
+++ lib/currencies.ts
@@ -0,0 +1,9 @@
+export const Currencies = [
+  {value: "USD", label: "$ Dollar", locale:"en-US"},
+  {value: "TWD", label: "NT$ Dollar", locale:"zh-TW"},
+  {value: "JPY", label: "¥ Yen", locale:"ja-JP"},
+  {value: "KRW", label: "₩ Won", locale:"ko-KR"},
+  {value: "EUR", label: "€ Euro", locale:"de-DE"},
+]
+
+export type Currency = (typeof Currencies)[0];
